8f998bb85b110ae614780d757fe272b978609627
TechPriest <yaminogakusei@gmail.com>
2019-11-08 16:49:23 +0200

    MINIDUMP_CALLBACK_TYPE enum

--- src/um/minidumpapi.rs
+++ src/um/minidumpapi.rs
@@ -4,3 +4,31 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
+//! Minidump API set. https://docs.microsoft.com/en-us/windows/win32/api/minidumpapiset/
+
+// use shared::ntdef::{HANDLE, HRESULT, PVOID, ULONG};
+
+ENUM!{enum MINIDUMP_CALLBACK_TYPE {
+    ModuleCallback,
+    ThreadCallback,
+    ThreadExCallback,
+    IncludeThreadCallback,
+    IncludeModuleCallback,
+    MemoryCallback,
+    CancelCallback,
+    WriteKernelMinidumpCallback,
+    KernelMinidumpStatusCallback,
+    RemoveMemoryCallback,
+    IncludeVmRegionCallback,
+    IoStartCallback,
+    IoWriteAllCallback,
+    IoFinishCallback,
+    ReadMemoryFailureCallback,
+    SecondaryFlagsCallback,
+    IsProcessSnapshotCallback,
+    VmStartCallback,
+    VmQueryCallback,
+    VmPreReadCallback,
+    VmPostReadCallback,
+}}
+
